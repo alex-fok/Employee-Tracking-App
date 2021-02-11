3f4f66d54c541b1eaceec61ef5e2e5c693a16e9e
Alexander Fok <foktmalexander@gmail.com>
2021-02-11 10:21:57 -0800

    Add partial functions for inserting new information

--- app.js
+++ app.js
@@ -17,9 +17,96 @@ const connection = mysql.createConnection(db_config);
 
 connection.connect( (err) => {
     if (err) throw err;
-    
+    updateDepartmentArr();
     startApp();
 });
+
+const updateDepartmentArr = () => {
+    return new Promise((resolve) => {
+        connection.query("SELECT name FROM department", (err, results) => {
+            departmentArr = results.map(el => el.name);
+            resolve();
+        });
+    });
+}
+
+const updateRoleArr = () => {
+    return new Promise((resolve) => {
+        connection.query("SELECT title FROM role", (err, results) => {
+            roleArr = results.map(el => el.titles);
+            resolve();
+        })
+    })
+}
+
+const inquireDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Department"
+        }
+    ]).then(answers => ({table: "department", row: { name: answers.department }}))
+}
+
+const inquireRole = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Title"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "Salary"
+        },
+        {
+            type: "input",
+            name: "department",
+            message: "Department"
+        }
+    ]).then(answers => ({
+        title: answers.title,
+        salary: parseFloat(answers.salary),
+        department: deparmentsArr.indexOf(answers.department)
+    }));
+}
+
+const inquireEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "First Name"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "Last Name"
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "Role"
+        },
+        {
+            type: "input",
+            name: "manager",
+            message: "Manager"
+        }
+    ]).then(answers => ({table: "employee", row: {
+        first_name: answers.firstName,
+        last_name: answers.lastName
+    }}))
+}
+
+const add = (data) => {
+    connection.query(`INSERT INTO ${data.table} SET ?`, data.row, (err) => {
+        if (err) throw err;
+    })
+}
+
 const view = (table) => {
     const query = `SELECT * from ${table}`;
     connection.query(query, (err, results) => {
@@ -28,12 +115,13 @@ const view = (table) => {
     })
 }
 
+const noExistingDepartment = () => console.log("No Existing Department");
 
 const updateEmployeeRole = () => {}
 const actions = {
-    "Add Department": () => {},
-    "Add Role": () => {},
-    "Add Employee": () => {},
+    "Add Department": () => {inquireDepartment().then(add)},
+    "Add Role": () => inquireRole.then(add),
+    "Add Employee": () => departmentArr.length > 0 ? inquireEmployee.then(add) : noExistingDepartment(),
     "View Deparments": () => view("department"),
     "View Roles": () => view("role"),
     "View Employees": () => view("employee"),
